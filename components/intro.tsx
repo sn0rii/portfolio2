bc52261a1ca679a9d85f6b371d1da48299f76f3a
Wojtek M <ojciecneo@gmail.com>
2024-01-14 08:14:45 +0100

    About me section done + heading component created

--- components/intro.tsx
+++ components/intro.tsx
@@ -49,7 +49,7 @@ export default function Intro() {
         </div>
       </div>
 
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -58,7 +58,7 @@ export default function Intro() {
         <span className="font-bold"> frontend developer</span> .I enjoy building{" "}
         <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
-      </motion.p>
+      </motion.h1>
 
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
