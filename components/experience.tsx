421a46c03abf328c93379864e41a0f6ff6e12a9e
Wojtek M <ojciecneo@gmail.com>
2024-01-19 16:31:06 +0100

    animation added to contact

--- components/experience.tsx
+++ components/experience.tsx
@@ -9,10 +9,12 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 
 import React from "react";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Experience() {
+  const { ref } = useSectionInView("Experience");
   return (
-    <section id="experience">
+    <section id="experience" className="scroll-mt-28 mb-28 sm:mb-40" ref={ref}>
       <SectionHeading>My experience</SectionHeading>
       <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
